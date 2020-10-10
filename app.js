adab1217a2b06208fb2782017263b61bb2067590
Anthony Diké <anthonydike@nyu.edu>
2020-10-10 17:13:16 -0400

    feat/calling '/bliss' opens modal

--- app.js
+++ app.js
@@ -10,7 +10,58 @@ app.command('/bliss', async ({ command, ack, say }) => {
   // Acknowledge command request
   await ack();
 
-  await say(`${command.text}!! 🌸`);
+  await say({
+    title: {
+      type: 'plain_text',
+      text: 'Modal Title',
+    },
+    submit: {
+      type: 'plain_text',
+      text: 'Submit',
+    },
+    blocks: [
+      {
+        type: 'input',
+        element: {
+          type: 'plain_text_input',
+          action_id: 'sl_input',
+          placeholder: {
+            type: 'plain_text',
+            text: 'Placeholder text for single-line input',
+          },
+        },
+        label: {
+          type: 'plain_text',
+          text: 'Label',
+        },
+        hint: {
+          type: 'plain_text',
+          text: 'Hint text',
+        },
+      },
+      {
+        type: 'input',
+        element: {
+          type: 'plain_text_input',
+          action_id: 'ml_input',
+          multiline: true,
+          placeholder: {
+            type: 'plain_text',
+            text: 'Placeholder text for multi-line input',
+          },
+        },
+        label: {
+          type: 'plain_text',
+          text: 'Label',
+        },
+        hint: {
+          type: 'plain_text',
+          text: 'Hint text',
+        },
+      },
+    ],
+    type: 'modal',
+  });
 });
 
 (async () => {
