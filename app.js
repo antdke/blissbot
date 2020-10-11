21214d705eec917ba7e313afcf68f93eba861f43
Anthony Diké <anthonydike@nyu.edu>
2020-10-11 16:36:28 -0400

    edit/called AllMiddleware arg 'client' as a param in app.command because client.views.open() is how modals are opened - *facepalm*

--- app.js
+++ app.js
@@ -6,144 +6,149 @@ const app = new App({
 });
 
 // slash commad
-app.command('/bliss', async ({ command, ack, say, payload, body }) => {
+app.command('/bliss', async ({ command, ack, payload, client }) => {
   // Acknowledge command request
   await ack();
 
-  await say({
-    type: 'modal',
-    trigger_id: body.trigger_id,
-    callback_id: 'modal-identifier',
-    title: {
-      type: 'plain_text',
-      text: 'BlissBot',
-      emoji: true,
-    },
-    submit: {
-      type: 'plain_text',
-      text: 'Submit',
-      emoji: true,
-      action_id: 'submit_button',
-    },
-    close: {
-      type: 'plain_text',
-      text: 'Cancel',
-      emoji: true,
-      action_id: 'close_button',
-    },
-    blocks: [
-      {
-        type: 'input',
-        block_id: 'app-request-form-title',
-        element: {
-          type: 'plain_text_input',
-          action_id: 'plain_text_input-action',
-        },
-        label: {
-          type: 'plain_text',
-          text: 'App/Service Name',
-          emoji: true,
-        },
+  try {
+    // Open modals with views.open using built-in client
+    const result = await client.views.open({
+      type: 'modal',
+      trigger_id: payload.trigger_id,
+      callback_id: 'modal-identifier',
+      title: {
+        type: 'plain_text',
+        text: 'BlissBot',
+        emoji: true,
       },
-      {
-        type: 'context',
-        block_id: 'app-request-form-context',
-        elements: [
-          {
+      submit: {
+        type: 'plain_text',
+        text: 'Submit',
+        emoji: true,
+        action_id: 'submit_button',
+      },
+      close: {
+        type: 'plain_text',
+        text: 'Cancel',
+        emoji: true,
+        action_id: 'close_button',
+      },
+      blocks: [
+        {
+          type: 'input',
+          block_id: 'app-request-form-title',
+          element: {
+            type: 'plain_text_input',
+            action_id: 'plain_text_input-action',
+          },
+          label: {
             type: 'plain_text',
-            text: 'Ex: "Notion", "HubSpot", "AWS", etc.',
+            text: 'App/Service Name',
             emoji: true,
           },
-        ],
-      },
-      {
-        type: 'input',
-        block_id: 'is-this-critical-radio-buttons',
-        element: {
-          type: 'radio_buttons',
-          options: [
+        },
+        {
+          type: 'context',
+          block_id: 'app-request-form-context',
+          elements: [
             {
-              text: {
-                type: 'plain_text',
-                text: 'Yes',
-                emoji: true,
-              },
-              value: 'value-0',
-            },
-            {
-              text: {
-                type: 'plain_text',
-                text: 'No',
-                emoji: true,
-              },
-              value: 'value-1',
+              type: 'plain_text',
+              text: 'Ex: "Notion", "HubSpot", "AWS", etc.',
+              emoji: true,
             },
           ],
-          action_id: 'radio_buttons-action',
         },
-        label: {
-          type: 'plain_text',
-          text: 'Is this critical to your job?',
-          emoji: true,
-        },
-      },
-      {
-        type: 'input',
-        block_id: 'who-will-manage-this',
-        element: {
-          type: 'multi_users_select',
-          placeholder: {
+        {
+          type: 'input',
+          block_id: 'is-this-critical-radio-buttons',
+          element: {
+            type: 'radio_buttons',
+            options: [
+              {
+                text: {
+                  type: 'plain_text',
+                  text: 'Yes',
+                  emoji: true,
+                },
+                value: 'value-0',
+              },
+              {
+                text: {
+                  type: 'plain_text',
+                  text: 'No',
+                  emoji: true,
+                },
+                value: 'value-1',
+              },
+            ],
+            action_id: 'radio_buttons-action',
+          },
+          label: {
             type: 'plain_text',
-            text: 'Select users',
+            text: 'Is this critical to your job?',
             emoji: true,
           },
-          action_id: 'multi_users_select-action',
         },
-        label: {
-          type: 'plain_text',
-          text: 'Who will manage this?',
-          emoji: true,
-        },
-      },
-      {
-        type: 'context',
-        block_id: 'who-will-manage-this-context',
-        elements: [
-          {
+        {
+          type: 'input',
+          block_id: 'who-will-manage-this',
+          element: {
+            type: 'multi_users_select',
+            placeholder: {
+              type: 'plain_text',
+              text: 'Select users',
+              emoji: true,
+            },
+            action_id: 'multi_users_select-action',
+          },
+          label: {
             type: 'plain_text',
-            text:
-              'Point of contact for IT to ask if they should renew or terminate the service.',
+            text: 'Who will manage this?',
             emoji: true,
           },
-        ],
-      },
-      {
-        type: 'input',
-        block_id: 'convince-me-textarea',
-        element: {
-          type: 'plain_text_input',
-          multiline: true,
-          action_id: 'plain_text_input-action',
         },
-        label: {
-          type: 'plain_text',
-          text: "Convince me it's worth the money",
-          emoji: true,
+        {
+          type: 'context',
+          block_id: 'who-will-manage-this-context',
+          elements: [
+            {
+              type: 'plain_text',
+              text:
+                'Point of contact for IT to ask if they should renew or terminate the service.',
+              emoji: true,
+            },
+          ],
         },
-      },
-      {
-        type: 'context',
-        block_id: 'convince-me-textarea-context',
-        elements: [
-          {
+        {
+          type: 'input',
+          block_id: 'convince-me-textarea',
+          element: {
+            type: 'plain_text_input',
+            multiline: true,
+            action_id: 'plain_text_input-action',
+          },
+          label: {
             type: 'plain_text',
-            text: 'Briefly explain why we should spend $ on this?',
+            text: "Convince me it's worth the money",
             emoji: true,
           },
-        ],
-      },
-    ],
-  });
+        },
+        {
+          type: 'context',
+          block_id: 'convince-me-textarea-context',
+          elements: [
+            {
+              type: 'plain_text',
+              text: 'Briefly explain why we should spend $ on this?',
+              emoji: true,
+            },
+          ],
+        },
+      ],
+    });
+  } catch (error) {
+    console.error(error);
+  }
 });
 
 (async () => {
